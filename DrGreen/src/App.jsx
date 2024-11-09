551d1bda52e41448285e3af8558a6a883501252f
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-11-09 13:55:11 -0500

    Update App.jsx

--- DrGreen/src/App.jsx
+++ DrGreen/src/App.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center mt-10">
-      <h1 className="font-extrabold text-white text-3xl">Dr.Green🌱</h1>
+    <div className="flex flex-col items-center">
+      <section className="mt-10">
+        <h1 className="font-extrabold text-white text-3xl">Dr.Green🌱</h1>
+      </section>
+
+      <section className="">
+
+      </section>
     </div>
   );
 };
