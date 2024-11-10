d14e600525f059d3310adfad1217614dee1c8519
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2024-11-09 21:23:41 -0500

    fix link

--- DrGreen/src/App.jsx
+++ DrGreen/src/App.jsx
@@ -1,53 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const uploadImage = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertBase64(file);
+  };
 
-  const uploadFiles = () =>{
-    document.getElementById('selectFile').click()
-  }
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
 
-  const InfoButton = () => {
-    const [showPopup, setShowPopup] = useState(false);
-
-    const handleClick = () => {
-      setShowPopup(true);
-    };
-
-    const handleOutsideClick = (event) => {
-      if (showPopup) {
-        setShowPopup(false);
-      }
-    };
-
-    useEffect(() => {
-      document.addEventListener('click', handleOutsideClick);
-
-      return () => {
-        document.removeEventListener('click', handleOutsideClick);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
       };
-    }, [showPopup]);
 
-    return (
-      <div className="flex flex-col items-center">
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            handleClick();
-          }}
-          className="bg-white text-black w-12 h-12 rounded-full absolute top-4 right-4 shadow-md hover:shadow-lg hover:bg-gray-200">
-          i
-        </button>
-
-        {showPopup && (
-          <div
-            className="absolute top-16 right-4 bg-opacity-80 bg-gray-600 p-4 w-72 h- auto rounded shadow-lg"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <p className="text-gray-200 text-sm "> Upload an image of your plant to identify any potential diseases and receive recommended solutions for treatment!! </p>
-          </div>
-        )}
-      </div>
-    );
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
   };
 
   return (
@@ -56,15 +27,16 @@ const App = () => {
         <h1 className="font-extrabold text-green-700 text-3xl">Dr.Green🌱</h1>
       </section>
 
-      <section>
-        <InfoButton />
-      </section>
-
-      <section className="mt-10"> 
-        <button className="bg-opacity-70 bg-gray-600 w-[65vw] h-[65vh] text-[10rem]" onClick={uploadFiles} >
-          +
-        </button>
-        <input id="selectFile" type = "file" style={{display: "none"}} accept="image/png, image/jpeg, image/jpg" />
+      <section className="mt-10">
+        <input
+          id="fileInput"
+          type="file"
+          accept="image/png, image/jpeg, image/jpg"
+          onChange={(e) => {
+            uploadImage(e);
+          }}
+          className=""
+        />
       </section>
 
       <section className="mt-10 bg-yellow-100">
@@ -72,9 +44,6 @@ const App = () => {
       </section>
     </div>
   );
-
-
 };
 
 export default App;
-
